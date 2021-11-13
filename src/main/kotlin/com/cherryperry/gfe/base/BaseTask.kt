cf0f25faf68ba6288f00164ad66d94fa79fc7492
CherryPerry <ya.cherryperry@yandex.ru>
2021-11-13 21:48:39 +0000

    Internal

--- src/main/kotlin/com/cherryperry/gfe/base/BaseTask.kt
+++ src/main/kotlin/com/cherryperry/gfe/base/BaseTask.kt
@@ -4,9 +4,11 @@ import com.cherryperry.gfe.FileEncryptExtension
 import com.cherryperry.gfe.GROUP_NAME
 import com.cherryperry.gfe.fileEncryptPluginExtension
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Internal
 
 abstract class BaseTask : DefaultTask() {
 
+    @get:Internal
     protected val fileEncryptPluginExtension: FileEncryptExtension = project.fileEncryptPluginExtension
 
     init {
