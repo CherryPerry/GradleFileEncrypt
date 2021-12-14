058441fa5ce8d05a6b1e9ff055fc684c733288f1
CherryPerry <ya.cherryperry@yandex.ru>
2021-12-14 20:21:33 +0000

    Use both systemProperty and environmentVariable

--- src/main/kotlin/com/cherryperry/gfe/providers/SystemPropertyPasswordProvider.kt
+++ src/main/kotlin/com/cherryperry/gfe/providers/SystemPropertyPasswordProvider.kt
@@ -2,13 +2,13 @@ package com.cherryperry.gfe.providers
 
 import org.gradle.api.provider.ProviderFactory
 
-internal class EnvironmentPasswordProvider(
+internal class SystemPropertyPasswordProvider(
     private val providersFactory: ProviderFactory,
 ) : PasswordProvider {
     override val password: CharArray?
-        get() = providersFactory.environmentVariable(ENVIRONMENT_KEY).forUseAtConfigurationTime().orNull?.toCharArray()
+        get() = providersFactory.systemProperty(SYSTEM_PROPERTY_KEY).forUseAtConfigurationTime().orNull?.toCharArray()
 
     companion object {
-        const val ENVIRONMENT_KEY = "GFE_PASSWORD"
+        const val SYSTEM_PROPERTY_KEY = "GFE_PASSWORD"
     }
 }
